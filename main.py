3528901b8b144f5e40bcf2bbb9e1d26d189c4114
jin <jin.544@osu.edu>
2017-05-29 16:35:39 -0400

    device follows args

--- main.py
+++ main.py
@@ -107,8 +107,8 @@ for xfold in range(args.xfolds):
     word_field = data.Field(lower=True, fix_length=3)
 
     label_field = data.Field(sequential=False)
-    train_iter, dev_iter, test_iter = vp(text_field, label_field, foldid=xfold, device=-1, repeat=False, shuffle=False)
-    train_iter_word, dev_iter_word, test_iter_word = vp(word_field, label_field, foldid=xfold, device=-1, repeat=False, shuffle=False)
+    train_iter, dev_iter, test_iter = vp(text_field, label_field, foldid=xfold, device=args.device, repeat=False, shuffle=False)
+    train_iter_word, dev_iter_word, test_iter_word = vp(word_field, label_field, foldid=xfold, device=args.device, repeat=False, shuffle=False)
 
     # update args and print
     args.embed_num = len(text_field.vocab)
