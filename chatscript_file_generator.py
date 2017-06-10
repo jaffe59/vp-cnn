fb2db7c6f0d96bbec37bd18ff3fd3f3ab31770ba
jin <jin.544@osu.edu>
2017-06-09 22:57:38 -0400

    converting to numpy

--- chatscript_file_generator.py
+++ chatscript_file_generator.py
@@ -61,7 +61,8 @@ def print_test_features(tensor, confidence, target, dialogue_indices, labels, in
         item = []
         item_id = start_id+ind
         dialogue_index, turn_index = dialogue_indices[item_id]
-        item += [dialogue_indices, turn_index]
+        item.append(dialogue_indices)
+        item.append(turn_index)
         item.append(labels[predicted[ind]])
         item.append(str(val))
         item.append(probs[ind])
