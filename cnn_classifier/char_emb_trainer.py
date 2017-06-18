af728a71044d320c183901845fa05a98a2fdc412
jin <jin.544@osu.edu>
2017-06-18 01:24:19 -0400

    fix char embedding file format

--- cnn_classifier/char_emb_trainer.py
+++ cnn_classifier/char_emb_trainer.py
@@ -40,8 +40,10 @@ def printout_embeds(char_embs, char_embs_file):
     with open(char_embs_file, 'w') as o:
         for word in char_embs.vocab:
             arr = np.array_str(char_embs[word])
+            arr = arr.replace('\n', '')
+            arr = arr.replace('  ', ' ')
             arr = arr[1:-1]
-            print(word+' '+arr, file=o)
+            print(word+arr, file=o)
 
 
 if __name__ == '__main__':
